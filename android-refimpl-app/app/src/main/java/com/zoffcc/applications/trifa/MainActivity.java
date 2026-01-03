cca4e86710d5f1d5b0e27c79bbd36979de3755ba
zoff99 <zoff99@users.noreply.github.com>
2026-01-03 20:08:11 +0100

    increase location interpolation steps to make the movement more smooth

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
@@ -275,7 +275,7 @@ public class MainActivity extends BaseProtectedActivity
 
     static boolean DEBUG_THREAD_STARTED = false;
     static boolean GEO_TIME_THREAD_STARTED = false;
-    static final int INTERPOLATE_POS_STEPS = 5;
+    static final int INTERPOLATE_POS_STEPS = 10;
 
     static TextView mt = null;
     static ImageView top_imageview = null;
