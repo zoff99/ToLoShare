46a5200a877582849d07c8141d82bb1fe6187f36
zoff99 <zoff99@users.noreply.github.com>
2026-01-18 10:53:17 +0100

    endable also mock location for debugging

--- tools/debug__enable_permissions.sh
+++ tools/debug__enable_permissions.sh
@@ -8,9 +8,12 @@ export _HOME_
 basedir="$_HOME_""/../"
 
 f1="android-refimpl-app/app/src/main/AndroidManifest.xml"
+f2="android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java"
 
 cd "$basedir"
 
 sed -i -e 's#<!--DEBUG##g' "$f1"
 sed -i -e 's#DEBUG-->##g' "$f1"
+
+sed -i -e 's#//\*\*MOCK\*\*//##g' "$f2"
 
